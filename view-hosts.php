f6464aa3cfbcfbadd83a141b019c8f7e646b812d
suthernfriend <public@janpeterkoenig.com>
2017-04-03 00:26:18 +0300

    removed file

--- view-hosts.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$hs = json_decode(file_get_contents("hosts.json"), true);
-
-foreach ($hs as $h) {
-	echo str_pad($h["ip"], 40) . " - " . str_pad($h["port"], 7) . " - " . $h["host"] . "\n";
-}
